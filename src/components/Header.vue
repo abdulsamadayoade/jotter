f947c62e6487a0d8fdf08d4600ac63c4832a4fbf
Abdulsamad Ayoade <ayoadesamad@gmail.com>
2022-12-12 02:13:41 +0100

    get and display total jot count

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,15 @@
 <script setup>
+  import { computed } from '@vue/reactivity';
+  import { useJotStore } from '../stores/jotStore';
 
+  const store = useJotStore();
+  const jotCount = computed(() => store.jots.length);
 </script>
 
 <template>
   <header class="header">
     <h1 class="header__heading">My Jotter App</h1>
     <p class="header__paragraph--first">This is a simple app for jotting things down.</p>
-    <p class="header__paragraph--second">Total Jot Count: (50)</p>
+    <p class="header__paragraph--second">Total Jot Count: ({{ jotCount }})</p>
   </header>
 </template>
